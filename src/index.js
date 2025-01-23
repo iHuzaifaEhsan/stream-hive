770cb7de86844c7fb5b0f1423aaf894bb72ec3e0
iHuzaifaEhsan <iamhuzaifaehsan@gmail.com>
2025-01-23 08:02:55 +0500

    added middlewares

--- src/index.js
+++ src/index.js
@@ -1,6 +1,19 @@
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config({ path: "./env" });
 
-connectDB();
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(` SERVER IS RUNNING AT PORT : ${process.env.PORT}`);
+      app.on("error", (error) => {
+        console.log("ERR: ", error);
+        throw error;
+      });
+    });
+  })
+  .catch((err) => {
+    console.log("Mongo DB connection failed !!! ", err);
+  });
